40a8686b64c9cc8fcff00e16ccbdebe6fff2d38c
Samuel Marvin <97563317+SLMarv@users.noreply.github.com>
2024-10-31 14:17:52 +0000

    Switch to factorial formula

--- test/test.js
+++ test/test.js
@@ -1,14 +1,13 @@
 var main = require('../index');
 var assert = require('assert');
-describe('Regular fibonacci value', function() {
-    it('should return 5 for Fib(5)', function() {
-        assert.strictEqual(main.fibonacci(5), 5);
+describe('Regular factorial value', function() {
+    it('should return', function() {
+        assert.strictEqual(main.factorial(5), 120);
       });
 });
 
-describe('First two terms', function() {
-  it('should return 0 and 1 for first two terms', function() {
-      assert.strictEqual(main.fibonacci(0), 0);
-      assert.strictEqual(main.fibonacci(1), 1);
+describe('First term', function() {
+  it('should return 1 for first term', function() {
+      assert.strictEqual(main.factorial(0), 1);
     });
 });
